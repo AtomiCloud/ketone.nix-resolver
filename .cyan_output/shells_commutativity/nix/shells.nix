5eec60b4463abc3cf9957cbffbd80e38077707c1
adelphi-liong <adelphi@atomi.cloud>
2026-03-24 11:42:27 -0700

    chore: remove test helpers and generated outputs, update gitignore [CU-86ex0n0y1]
    - Remove .cyan_output/ (generated by cyanprint test resolver)
    - Remove scripts/test-error-paths.ts, test-precommit.ts, regen-snapshots.ts
    - Add .cyan_output/ and .cyan_backup*/ to .gitignore
    - Update template files (.cyan_state.yaml, .dockerignore)

--- .cyan_output/shells_commutativity/nix/shells.nix
+++
@@ -1,8 +0,0 @@
-{ env, packages, pkgs, shellHook }:
-with env;
-{
-  default = pkgs.mkShell {
-    buildInputs = dev ++ infra ++ lint ++ main ++ system;
-    inherit shellHook;
-  };
-}
--- .cyan_output/shells_single_file/nix/shells.nix
+++
@@ -1,8 +0,0 @@
-{ env, packages, pkgs, shellHook }:
-with env;
-{
-  default = pkgs.mkShell {
-    buildInputs = dev ++ main ++ system;
-    inherit shellHook;
-  };
-}
