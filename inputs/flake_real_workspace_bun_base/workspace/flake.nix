50cfe0aadfd321a0f5abad0d7d3ea645f0fd33cb
kirinnee <kirinnee97@gmail.com>
2026-08-07 00:47:52 +0000

    fix: preserve flake output bindings and comments

---
+++ inputs/flake_real_workspace_bun_base/workspace/flake.nix
@@ -0,0 +1,84 @@
+{
+  inputs = {
+    # util
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+    # registry
+    #
+    # nixpkgs inputs pin exact commits;
+    # atomipkgs deliberately floats on the v4 major line.
+
+    # nixos-unstable @ 2026-08-06
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/b7c2ada94fe99c15b0dbcf4d11fd7850b957a436";
+    # nixos-26.05 (Yarara) @ 2026-08-06
+    nixpkgs-2605.url = "github:NixOS/nixpkgs/445d861c6d31b4af0c79d8d4be2331f762a361d7";
+    # Rev-pinned during the 2026-08-06 Actions outage (no release could cut); restore to /v4 after.
+    atomipkgs.url = "github:AtomiCloud/nix-registry/0d75f6006677c591605e4eff4a84419695e02526";
+  };
+  outputs =
+    {
+      self,
+
+      # utils
+      flake-utils,
+      treefmt-nix,
+      pre-commit-hooks,
+
+      # registries
+      atomipkgs,
+      nixpkgs-2605,
+      nixpkgs-unstable,
+
+    }@inputs:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs-2605 = nixpkgs-2605.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+        atomi = atomipkgs.packages.${system};
+        pre-commit-lib = pre-commit-hooks.lib.${system};
+      in
+      let
+        pkgs = pkgs-2605;
+      in
+      with rec {
+        pre-commit = import ./nix/pre-commit.nix {
+          inherit
+            packages
+            pkgs
+            pre-commit-lib
+            formatter
+            env
+            ;
+        };
+        formatter = import ./nix/fmt.nix {
+          inherit treefmt-nix pkgs;
+        };
+        packages = import ./nix/packages.nix {
+          inherit pkgs-2605 pkgs-unstable atomi;
+        };
+        env = import ./nix/env.nix {
+          inherit pkgs packages;
+        };
+        devShells = import ./nix/shells.nix {
+          inherit pkgs env packages;
+          shellHook = checks.pre-commit-check.shellHook;
+        };
+        checks = {
+          pre-commit-check = pre-commit;
+          format = formatter;
+        };
+      };
+      {
+        inherit
+          checks
+          formatter
+          packages
+          devShells
+          ;
+      }
+    ));
+
+}
